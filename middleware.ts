effa6362a006d35cdd93e741efd88bb9fd3803a3
Akshat Talwar <talwara1@uci.edu>
2025-01-26 07:37:45 -0800

    Travel Recti

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,8 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Exclude /_not-found from Clerk checks
+    '/((?!_next|_not-found|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
 };
-
